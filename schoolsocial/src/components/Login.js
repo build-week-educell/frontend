3cddb1f1230ac974760c128675448ed7815bf1a0
larrysimiyu <larrysimiyu@Larrys-MacBook-Pro.local>
2019-03-11 23:45:16 -0700

    log in functioning

---
+++ schoolsocial/src/components/Login.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  
+            username: '',
+            password: ''
+         }
+    }
+    
+    handleInputChange = event => {
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+    }
+
+    render() { 
+        return ( 
+            <form>
+                <input 
+                    type='text'
+                    placeholder='enter email'
+                    value={this.state.username}
+                    onChange={this.handleInputChange}
+                    name='username'
+                />
+                <input 
+                    type='password'
+                    placeholder='enter password'
+                    value={this.state.password}
+                    onChange={this.handleInputChange}
+                    name="password"
+                />
+
+                <button>Log In</button>
+
+            </form>
+         );
+    }
+}
+ 
+export default Login;
