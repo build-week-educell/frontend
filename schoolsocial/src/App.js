1194ec03e6add84ef6dadd503419064da88bab84
larrysimiyu <larrysimiyu@Larrys-MacBook-Pro.local>
2019-03-12 14:15:21 -0700

    connect to backend

--- schoolsocial/src/App.js
+++ schoolsocial/src/App.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 import StudentForm from './components/Student Components/StudentForm';
+import axios from 'axios';
 
 class App extends Component {
+
+  test = event =>{
+    axios
+      .get('https://educell.herokuapp.com/')
+      .then( response => console.log(response) )
+      .catch( error => console.log(error) )
+  }
+  
   render() {
     return (
       <div className="App">
+        <button onClick={this.test}>TEST</button>
         <StudentForm />
       </div>
     );
