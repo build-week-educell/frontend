8088b638fa0221917a95f2da7dc9a3a95e8d8334
SabrinaGear <sabrinagear.ft@gmail.com>
2019-03-14 21:22:47 -0500

    routing

--- schoolsocial/src/components/EditStudent.js
+++ schoolsocial/src/components/EditStudent.js
@@ -1,182 +1,185 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class EditStudent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            grade: 0,
-            background: '',
-            status: '',
-            age: 0,
-            insurance: false,
-            insuranceExp: '',
-            birthCertificate: false,
-            specialNeeds: '',
-            representative: '',
-            contactInfo: '',
-            id: null
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      grade: 0,
+      background: "",
+      status: "",
+      age: 0,
+      insurance: false,
+      insuranceExp: "",
+      birthCertificate: false,
+      specialNeeds: "",
+      representative: "",
+      contactInfo: "",
+      id: null
+    };
+  }
 
-    handleInputChange = event => {
+  handleInputChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  componentDidMount() {
+    const id = this.props.location.pathname.split("/")[2];
+    axios
+      .get(`https://educell.herokuapp.com/api/students/${id}`, {
+        headers: { Authorization: localStorage.getItem("token") }
+      })
+      .then(response => {
+        let student = response.data[0];
         this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+          name: student.name,
+          grade: student.grade,
+          background: student.background,
+          status: student.status,
+          age: student.age,
+          insurance: student.insurance,
+          insuranceExp: student.insuranceExp,
+          birthCertificate: student.birthCertificate,
+          specialNeeds: student.specialNeeds,
+          representative: student.representative,
+          contactInfo: student.contactInfo,
+          id: id
+        });
+      })
+      .catch(error => console.log(error));
+  }
 
-    componentDidMount() {
-        const id = this.props.location.pathname.split("/")[2]
-        axios
-            .get(`https://educell.herokuapp.com/api/students/${id}`, { headers: { Authorization: localStorage.getItem('token') } })
-            .then(response => {
-                let student = response.data[0]
-                this.setState({
-                    name: student.name,
-                    grade: student.grade,
-                    background: student.background,
-                    status: student.status,
-                    age: student.age,
-                    insurance: student.insurance,
-                    insuranceExp: student.insuranceExp,
-                    birthCertificate: student.birthCertificate,
-                    specialNeeds: student.specialNeeds,
-                    representative: student.representative,
-                    contactInfo: student.contactInfo,
-                    id: id
-                })
-            })
-            .catch(error => console.log(error))
-    }
+  editStudent = event => {
+    event.preventDefault(); //anytime you use on submit for a form
 
-    editStudent = event => {
-        event.preventDefault() //anytime you use on submit for a form 
+    const {
+      name,
+      grade,
+      background,
+      status,
+      age,
+      insurance,
+      insuranceExp,
+      birthCertificate,
+      specialNeeds,
+      representative,
+      contactInfo
+    } = this.state;
 
-        const {
-            name,
-            grade,
-            background,
-            status,
-            age,
-            insurance,
-            insuranceExp,
-            birthCertificate,
-            specialNeeds,
-            representative,
-            contactInfo } = this.state;
+    let newStudent = {
+      name,
+      grade,
+      background,
+      status,
+      age,
+      insurance,
+      insuranceExp,
+      birthCertificate,
+      specialNeeds,
+      representative,
+      contactInfo
+    };
 
-        let newStudent = {
-            name,
-            grade,
-            background,
-            status,
-            age,
-            insurance,
-            insuranceExp,
-            birthCertificate,
-            specialNeeds,
-            representative,
-            contactInfo
+    axios
+      .put(
+        `https://educell.herokuapp.com/api/students/${this.state.id}`,
+        newStudent,
+        { headers: { Authorization: localStorage.getItem("token") } }
+      )
+      .then(response => {
+        if (response.status === 200) {
+          console.log(response.data);
+          //if registration is succesful which is this check, then redirect to log in.
+          alert("Student was updated");
+          this.props.history.push("/students");
+        } else {
+          throw new Error(); //error if something went wrong
         }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-        axios
-            .put(`https://educell.herokuapp.com/api/students/${this.state.id}`, newStudent, { headers: { Authorization: localStorage.getItem('token') } })
-            .then(response => {
-                if (response.status === 200) {
-                    console.log(response.data)
-                    //if registration is succesful which is this check, then redirect to log in.
-                    alert('Student was updated')
-                    
+  deleteStudent = () => {
+    axios
+      .delete(`https://educell.herokuapp.com/api/students/${this.state.id}`, {
+        headers: { Authorization: localStorage.getItem("token") }
+      })
+      .then(response => {
+        console.log(response);
+        alert("Student Was Deleted");
+      })
+      .then(() => this.props.history.goBack())
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-                }
-                else {
-                    throw new Error() //error if something went wrong 
-                }
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    }
+  //make booleans toggle button
+  //status 3 radio buttons 3 options currentstudent former student and visitor
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.addStudent}>
+          <input
+            type="text"
+            placeholder="Student Name"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+            name="name"
+          />
+          <br />
 
-    deleteStudent = () => {
-        axios
-            .delete(`https://educell.herokuapp.com/api/students/${this.state.id}`, { headers: { Authorization: localStorage.getItem('token') } })
-            .then(response => {
-                console.log(response);
-                alert("Student Was Deleted")
-            })
+          <h2>Student Age</h2>
+          <input
+            type="number"
+            placeholder="Student First Name"
+            value={this.state.age}
+            onChange={this.handleInputChange}
+            name="age"
+          />
+          <br />
 
-            .catch(err => {
-                console.log(err);
-            });
-    }
+          <h2>Insurance</h2>
+          <input
+            type="text"
+            placeholder="Y/N"
+            value={this.state.insurance}
+            onChange={this.handleInputChange}
+            name="insurance"
+          />
+          <br />
 
-    //make booleans toggle button
-    //status 3 radio buttons 3 options currentstudent former student and visitor
-    render() {
-        return (
-            <div>
-                <form
-                    onSubmit={this.addStudent}
-                >
-                    <input
-                        type='text'
-                        placeholder='Student Name'
-                        value={this.state.name}
-                        onChange={this.handleInputChange}
-                        name='name'
-                    />
-                    <br></br>
+          <h2>Birth Certificate</h2>
+          <input
+            type="text"
+            placeholder="Y/N"
+            value={this.state.birthCertificate}
+            onChange={this.handleInputChange}
+            name="birthCertificate"
+          />
+          <br />
 
-                    <h2>Student Age</h2>
-                    <input
-                        type='number'
-                        placeholder='Student First Name'
-                        value={this.state.age}
-                        onChange={this.handleInputChange}
-                        name='age'
-                    />
-                    <br></br>
+          <h2>Contact Information</h2>
+          <input
+            type="text"
+            placeholder="Enter Phone Number"
+            value={this.state.contactInfo}
+            onChange={this.handleInputChange}
+            name="contactInfo"
+          />
+          <br />
 
-                    <h2>Insurance</h2>
-                    <input
-                        type='text'
-                        placeholder='Y/N'
-                        value={this.state.insurance}
-                        onChange={this.handleInputChange}
-                        name='insurance'
-                    />
-                    <br></br>
-
-                    <h2>Birth Certificate</h2>
-                    <input
-                        type='text'
-                        placeholder='Y/N'
-                        value={this.state.birthCertificate}
-                        onChange={this.handleInputChange}
-                        name='birthCertificate'
-                    />
-                    <br></br>
-
-                    <h2>Contact Information</h2>
-                    <input
-                        type='text'
-                        placeholder='Enter Phone Number'
-                        value={this.state.contactInfo}
-                        onChange={this.handleInputChange}
-                        name='contactInfo'
-                    />
-                    <br></br>
-
-                    <button onClick={this.editStudent}>Submit</button>
-                    <button onClick={this.deleteStudent}>Delete</button>
-                </form>
-                
-
-            </div>
-        );
-    }
+          <button onClick={this.editStudent}>Submit</button>
+          <button onClick={this.deleteStudent}>Delete</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default EditStudent;
--- schoolsocial/src/components/Students.js
+++ schoolsocial/src/components/Students.js
@@ -1,52 +1,51 @@
-//show all students 
-//get request to all students 
-
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 class Students extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            students: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: []
+    };
+  }
 
-    componentDidMount() {
-        axios
-            .get('https://educell.herokuapp.com/api/students', { headers: { Authorization: localStorage.getItem('token') } })
-            .then( response => {
-                this.setState({
-                    students: response.data
-                })
-            })
-            .catch(error => console.log(error))
-    }
-    
-    render() { 
-        return ( 
-            <div>
-                
-                {this.state.students.map( student => {
-                    return (
-                        <div key={student.id}>
-                            <Link to={`/student/${student.id}`}>
-                                <div>{student.name}</div>
-                                <div>{student.status}</div>
-                                <div>{student.age}</div>
-                                <div>{student.insurance}</div>
-                                <div>{student.birthCertificate}</div>
-                                <div>{student.representative}</div>
-                                <div>{student.contactInfo}</div>
-                            </Link>
-                            
-                        </div>
-                    )
-                })}
-           </div>
-         );
-    }
+  componentDidMount() {
+    axios
+      .get("https://educell.herokuapp.com/api/students", {
+        headers: { Authorization: localStorage.getItem("token") }
+      })
+      .then(response => {
+        this.setState({
+          students: response.data
+        });
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.students.map(student => {
+          return (
+            <div key={student.id}>
+              <Link to={`/student/${student.id}`}>
+                <div class="card">
+                  <div>{student.name}</div>
+                  <div>{student.status}</div>
+                  <div>{student.age}</div>
+                  <div>{student.insurance}</div>
+                  <div>{student.birthCertificate}</div>
+                  <div>{student.representative}</div>
+                  <div>{student.contactInfo}</div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
- 
+
 export default Students;
